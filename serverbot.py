1c2adcc27a92cec9badabae9f240cad1c0457f61
DrgnFireYellow <DrgnFireYellow@users.noreply.github.com>
2024-02-18 10:45:14 -0500

    Use pycord instead of discord.py

--- serverbot.py
+++ serverbot.py
@@ -1,6 +1,4 @@
-import discord.ext.commands as discord_commands
 import discord
-from discord import app_commands
 import docker
 import json
 import os
@@ -16,8 +14,7 @@ dockerclient = docker.from_env()
 intents = discord.Intents.default()
 intents.message_content = True
 
-client = discord.Client(intents=intents)
-tree = app_commands.CommandTree(client)
+bot = discord.Bot()
 
 if os.path.exists("data.json"):
     with open("data.json") as datafile:
@@ -26,12 +23,11 @@ else:
     data = {"users": {}, "ids": []}
 
 
-@client.event
+@bot.event
 async def on_ready():
-    await tree.sync()
-    print(f"Logged in as {client.user}")
+    print(f"Logged in as {bot.user}")
 
-@tree.command(name="register")
+@bot.command(name="register")
 async def register(ctx):
     if ctx.author in data["users"].keys():
         await ctx.response.send_message("You are already registered.")
@@ -44,29 +40,30 @@ async def register(ctx):
             data['ids'].append(0)
         await ctx.response.send_message(f"Registered with id of {data['users'][ctx.author]}")
 
-@tree.command(name="create", description="Create a new Minecraft server")
+@bot.command(name="create", description="Create a new Minecraft server")
 async def create(ctx, server_name: str, server_version: str):
     dockerclient.containers.run("itzg/minecraft-server", detach=True, environment=[f"VERSION={server_version}", "EULA=TRUE"], ports={25565: 25565}, name=f"serverbot_{server_name}")
     await ctx.response.send_message(f'Created server `{server_name}` with Minecraft version {server_version}.')
 
-@tree.command(name="start", description="Start an existing Minecraft server")
+@bot.command(name="start", description="Start an existing Minecraft server")
 async def start(ctx, server_name: str):
     dockerclient.containers.get(f"serverbot_{server_name}").start()
     await ctx.response.send_message(f'Starting server `{server_name}`')
     
-@tree.command(name="run", description="Run a command on a running Minecraft server")
+@bot.command(name="run", description="Run a command on a running Minecraft server")
 async def run(ctx, server_name: str, command: str):
     dockerclient.containers.get(f"serverbot_{server_name}").exec_run(f"rcon-cli {command}")
     await ctx.response.send_message(f"Ran command `{command}` on server `{server_name}`")
 
 
-@tree.command(name="log", description="Show the most recent log output from a Minecraft server")
+@bot.command(name="log", description="Show the most recent log output from a Minecraft server")
 async def log(ctx, server_name: str):
-    await ctx.response.send_message(f"Most recent log output for server `{server_name}`: ```{dockerclient.containers.get(f'serverbot_{server_name}').logs()[-1000:].decode().replace("\\n", "\n")}```")
+    logoutput = dockerclient.containers.get(f'serverbot_{server_name}').logs()[-1000:].decode().replace('\\n', '\n')
+    await ctx.response.send_message(f"Most recent log output for server `{server_name}`: ```{logoutput}```")
 
-@tree.command(name="stop", description="Stop a running Minecraft server")
+@bot.command(name="stop", description="Stop a running Minecraft server")
 async def stop(ctx, server_name: str):
     dockerclient.containers.get(f"serverbot_{server_name}").stop()
     await ctx.response.send_message(f"Stopped server `{server_name}`")
 
-client.run(args.bot_token)
+bot.run(args.bot_token)
